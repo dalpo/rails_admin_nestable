e456fcedd336695cc1c5bebb1754f532b47edb94
Andrea Dal Ponte <dalpo85@gmail.com>
2012-09-19 10:16:58 +0200

    tree visualization fix

--- app/helpers/rails_admin/ancestry_helper.rb
+++ app/helpers/rails_admin/ancestry_helper.rb
@@ -3,14 +3,15 @@ module RailsAdmin
 
     def nested_tree_nodes(tree_nodes)
       tree_nodes.map do |tree_node, sub_tree_nodes|
-        li_class = 'dd-item dd3-item'
+        li_classes = 'dd-item dd3-item'
 
-        content_tag :li, class: li_class, :'data-id' => tree_node.id do
-          # output = link_to @model_config.with(object: tree_node).object_label, edit_path(@abstract_model, tree_node.id)
+        content_tag :li, class: li_classes, :'data-id' => tree_node.id do
+
           output = content_tag :div, 'drag', class: 'dd-handle dd3-handle'
           output+= content_tag :div, class: 'dd3-content' do
             link_to @model_config.with(object: tree_node).object_label, edit_path(@abstract_model, tree_node.id)
           end
+
           output+= content_tag :ol, nested_tree_nodes(sub_tree_nodes), class: 'dd-list' if sub_tree_nodes.any?
           output
         end
