edcdfb104269192f661fbb3106b168b20bbbdb3f
Andrea Dal Ponte <dalpo85@gmail.com>
2012-09-19 10:56:51 +0200

    tree visualization

--- app/helpers/rails_admin/ancestry_helper.rb
+++ app/helpers/rails_admin/ancestry_helper.rb
@@ -9,13 +9,20 @@ module RailsAdmin
 
           output = content_tag :div, 'drag', class: 'dd-handle dd3-handle'
           output+= content_tag :div, class: 'dd3-content' do
-            link_to @model_config.with(object: tree_node).object_label, edit_path(@abstract_model, tree_node.id)
+            content = link_to @model_config.with(object: tree_node).object_label, edit_path(@abstract_model, tree_node.id)
+            content+= content_tag :div, action_links(tree_node), class: 'pull-right links'
           end
 
           output+= content_tag :ol, nested_tree_nodes(sub_tree_nodes), class: 'dd-list' if sub_tree_nodes.any?
           output
         end
       end.join.html_safe
+    end
+
+    def action_links(model)
+      content_tag :ul, class: 'inline actions' do
+        menu_for :member, @abstract_model, model, true
+      end
     end
 
   end
