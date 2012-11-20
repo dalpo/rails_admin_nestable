012f0a8d7403185cd6c350b5da9e654919993ff2
Andrea Dal Ponte <dalpo85@gmail.com>
2012-11-20 11:55:29 +0100

    asset precompile

--- lib/rails_admin_nestable/engine.rb
+++ lib/rails_admin_nestable/engine.rb
@@ -2,7 +2,7 @@ module RailsAdminNestable
   class Engine < ::Rails::Engine
 
     initializer "RailsAdminNestable precompile hook", group: :all do |app|
-      app.config.assets.precompile += %w(jquery.nestable.js)
+      app.config.assets.precompile += %w(rails_admin/rails_admin_nestable.js)
     end
 
   end
