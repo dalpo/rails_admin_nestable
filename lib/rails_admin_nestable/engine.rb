0b58a15922f681735f9ab893eb65bd90a170008d
Andrea Dal Ponte <dalpo85@gmail.com>
2012-11-20 11:32:20 +0100

    Enabling assets precompiling

--- lib/rails_admin_nestable/engine.rb
+++ lib/rails_admin_nestable/engine.rb
@@ -1,4 +1,10 @@
 module RailsAdminNestable
   class Engine < ::Rails::Engine
+
+    # Enabling assets precompiling
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile += %w( jquery.nestable.js )
+    end
+
   end
 end
