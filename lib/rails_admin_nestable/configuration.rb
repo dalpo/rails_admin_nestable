567dc569fe2bac28d7158745681cc4f1270c895f
Andrea Dal Ponte <info@andreadalponte.com>
2013-04-11 17:28:37 +0200

    fx configuration

--- lib/rails_admin_nestable/configuration.rb
+++ lib/rails_admin_nestable/configuration.rb
@@ -1,6 +1,9 @@
 module RailsAdminNestable
   class Configuration
 
+    TREE_DEFAULT_OPTIONS = { enable_callback: false }
+    LIST_DEFAULT_OPTIONS = { position_field: :position, max_depth: 1, enable_callback: false }
+
     def initialize(abstract_model)
       @abstract_model = abstract_model
     end
@@ -15,21 +18,23 @@ module RailsAdminNestable
 
     def options
       if tree?
-        @nestable_options ||= (tree.class == Hash ? tree : {
-          enable_callback: false
-        })
+        @nestable_options ||= self.tree_options
       elsif list?
-        @nestable_options ||= {
-          position_field: :position,
-          max_depth: 1,
-          enable_callback: false
-        }.merge(list.class == Hash ? list : {})
+        @nestable_options ||= self.list_options
       end
 
       @nestable_options || {}
     end
 
   protected
+
+    def tree_options
+      tree.class == Hash ? TREE_DEFAULT_OPTIONS.merge(tree) : TREE_DEFAULT_OPTIONS
+    end
+
+    def list_options
+      LIST_DEFAULT_OPTIONS.merge(list.class == Hash ? list : {})
+    end
 
     def tree
       @nestable_tree ||= ::RailsAdmin::Config.model(@abstract_model.model).nestable_tree
