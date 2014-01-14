aa06fb9b0518f759aa320d57aa0d036f9b2194d9
Justin <dieunb@gmail.com>
2014-01-14 13:52:45 +0700

    Readd js, css

--- app/assets/javascripts/rails_admin/custom/ui.js
+++
@@ -1,3 +0,0 @@
-//= require jquery.nestable
-//= require rails_admin_nestable
-//= require_tree .
--- lib/rails_admin_nestable/engine.rb
+++ lib/rails_admin_nestable/engine.rb
@@ -2,7 +2,7 @@ module RailsAdminNestable
   class Engine < ::Rails::Engine
 
     initializer "RailsAdminNestable precompile hook", group: :all do |app|
-      app.config.assets.precompile += %w(rails_admin/custom/ui.js rails_admin/custom/theming.scss)
+      app.config.assets.precompile += %w(rails_admin/rails_admin_nestable.js rails_admin/jquery.nestable.js rails_admin/rails_admin_nestable.css)
     end
 
     initializer 'Include RailsAdminNestable::Helper' do |app|
